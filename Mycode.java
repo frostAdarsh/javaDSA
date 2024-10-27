2284e3102812d213e6a315b5b683a92e11e0a97b
frostAdarsh <adarshmani017@gmail.com>
2024-10-27 16:13:32 +0530

    done

---
+++ Mycode.java
@@ -0,0 +1,12 @@
+public class  Mycode{
+    public static void main(String[] args) {
+        // System.out.println("hello");
+        // int a = 23;
+        // System.out.println(a);
+        String s = "java";
+        System.out.println(s);
+        final int a = 34242;
+        System.out.println(a);
+       
+    }
+}
