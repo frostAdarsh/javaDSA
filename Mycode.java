f87059abeb9f152659d22781b47aa073b141ded9
frostAdarsh <adarshmani017@gmail.com>
2024-10-28 20:59:34 +0530

    done 2

--- Mycode.java
+++ Mycode.java
@@ -1,12 +1,29 @@
-public class  Mycode{
+
+
+public class Mycode {
+
     public static void main(String[] args) {
         // System.out.println("hello");
         // int a = 23;
         // System.out.println(a);
-        String s = "java";
-        System.out.println(s);
-        final int a = 34242;
-        System.out.println(a);
-       
+        // String s = "java";
+        // System.out.println(s);
+        // final int a = 34242;
+        // System.out.println(a);
+        // Scanner is use for input
+        // Scanner sc = new Scanner(System.in);
+
+        // System.err.println("What is your name");
+        // String name = sc.nextLine();
+        // System.err.println("What is your age");
+        // int age = sc.nextInt();
+        // System.err.println("Hi " + name + "you are " + age);
+        // sc.close();
+       int a = 10;
+       int b = a++;
+       int c = b;
+       System.out.println(b);
+       System.out.println(c);
+
     }
 }
