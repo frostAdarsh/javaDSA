0bbb68a5914879744f6ecede9fe953c288355044
frostAdarsh <adarshmani017@gmail.com>
2024-10-29 20:54:53 +0530

    done 3

--- Mycode.java
+++ Mycode.java
@@ -1,29 +1,50 @@
 
-
 public class Mycode {
 
+    // public static void main(String[] args) {
+    // System.out.println("hello");
+    // int a = 23;
+    // System.out.println(a);
+    // String s = "java";
+    // System.out.println(s);
+    // final int a = 34242;
+    // System.out.println(a);
+    // Scanner is use for input
+    // Scanner sc = new Scanner(System.in);
+    // System.err.println("What is your name");
+    // String name = sc.nextLine();
+    // System.err.println("What is your age");
+    // int age = sc.nextInt();
+    // System.err.println("Hi " + name + "you are " + age);
+    // sc.close();
+    //    int a = 10;
+    //    int b = a++;
+    //    int c = b;
+    //    System.out.println(b);
+    //    System.out.println(c);
+    // }
     public static void main(String[] args) {
-        // System.out.println("hello");
-        // int a = 23;
-        // System.out.println(a);
-        // String s = "java";
-        // System.out.println(s);
-        // final int a = 34242;
-        // System.out.println(a);
-        // Scanner is use for input
-        // Scanner sc = new Scanner(System.in);
+        // int age =26;
+        // int aGe = 12;
+        // int marks = 98;
+        // int secondMarks = 23;
+        // int $marks = 87;
+        // int _marks = 87;
 
-        // System.err.println("What is your name");
-        // String name = sc.nextLine();
-        // System.err.println("What is your age");
-        // int age = sc.nextInt();
-        // System.err.println("Hi " + name + "you are " + age);
-        // sc.close();
-       int a = 10;
-       int b = a++;
-       int c = b;
-       System.out.println(b);
-       System.out.println(c);
+        //------------Variable primitive type---------
+        // boolean isPassed = true;
+        // int count = 12;
+        // byte marks = 13;
+        // short studentCount = 1234;
+        // float pi = 3.14f;
+        // double secondPi = 3.14;
+        // char myLetter = 'A';
+        // System.err.println(myLetter);
+        //------------implicit/explicite--------
+        int count = 167;
+        byte byteConunt = (byte) count;
+        System.err.println(byteConunt);
 
     }
+
 }
